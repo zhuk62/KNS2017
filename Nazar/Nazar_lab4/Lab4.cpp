636a65f43c771c1713464d52ff058c397c2cc6ed
deimss <deimss95@gmail.com>
2017-05-10 15:04:58 +0300

    Add files for lab4

---
+++ Nazar/Nazar_lab4/Lab4.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define FIRST -10
+#define LAST 53
+
+using namespace std;
+
+
+const size_t X = 10;
+
+
+double fitness(double &n)
+{
+	return 62 - n - 86*n*n + 2*n*n*n;
+
+}
+
+void selection_max(double a[])
+{
+	for (size_t i = 0; i < X; ++i)
+		for (size_t j = i + 1; j < X; ++j)
+			if (fitness(a[j]) < fitness(a[i]))
+				swap(a[i], a[j]);
+
+	copy(a + X / 2, a + X, a);
+}
+
+void selection_min(double a[])
+{
+	for (size_t i = 0; i < X; ++i)
+		for (size_t j = i + 1; j < X; ++j)
+			if (fitness(a[j]) > fitness(a[i]))
+				swap(a[i], a[j]);
+
+	copy(a + X / 2, a + X, a);
+}
+
+double mutation_min(double &t)
+{
+
+	do { t += ((((double)(rand() % 2)) / 100 == 0.01) ? 0.1 : -0.01);
+	} 
+	while (t == 0 || t<FIRST || t>LAST);
+
+
+	return t;
+}
+double mutation_max(double &t)
+{
+
+	do { t += ((((double)(rand() % 2)) / 100 == 0.01) ? 0.01 : -0.01);
+	} 
+	while (t == 0 || t<FIRST || t>LAST);
+
+
+	return t;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+	double data_base[X] = { 0 };
+	double data_base2[X] = { 0 };
+
+
+	for (int rounds = 0; rounds < 500; rounds++)
+	{
+		for (size_t i = 0; i < X; ++i)
+			data_base[i] = mutation_max(data_base[i]);
+		selection_max(data_base);
+	}
+
+	for (int rounds = 0; rounds < 500; rounds++)
+	{
+		for (size_t i = 0; i < X; ++i)
+			data_base2[i] = mutation_min(data_base2[i]);
+		selection_min(data_base2);
+	}
+	double ymax = fitness(data_base[4]);
+	double ymin = fitness(data_base2[4]);
+
+	cout << "Пошук максимального значення функцiї" << endl;
+	cout << "Обчисленення [х,y] на всiх iтерацiях:" << endl;
+	for (size_t i = 0; i < X; i++)
+		cout << "Iтерацiя №" << i+1 << ": x=" << data_base[i] << ", y=" << fitness(data_base[i]) << endl;
+
+
+	cout << "При x=" << data_base[9] << endl;
+	cout << "y_max=" << ymax;
+	cout << endl;
+	cout << endl;
+	cout << "Пошук мiнiмального значення функцiї" << endl;
+	cout << "Обчисленення [х,y] на всiх iтерацiях:" << endl;
+	for (size_t i = 0; i < X; i++)
+		cout << "Iтерацiя №" << i+1 << ": x=" << data_base2[i] << ", y=" << fitness(data_base2[i]) << endl;
+
+	cout << "При x=" << data_base2[9] << endl;
+	cout << "y_min=" << ymin;
+	cin.get();
+	return 0;
+}
